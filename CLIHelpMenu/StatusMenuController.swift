b5f060471271b650b55a82ee054a0d29c1e9a8b7
arajput <arajput@getwellnetwork.com>
2018-10-06 23:13:51 -0400

    changed status bar icon

--- CLIHelpMenu/StatusMenuController.swift
+++ CLIHelpMenu/StatusMenuController.swift
@@ -2,7 +2,7 @@
 
 import Cocoa
 
-class StatusMenuController: NSObject, PreferencesWindowDelegate {    
+class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
 
     var preferencesWindow: PreferencesWindow!
@@ -15,97 +15,67 @@ class StatusMenuController: NSObject, PreferencesWindowDelegate {
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
-        preferencesWindow = PreferencesWindow()
-        preferencesWindow.delegate = self
-        
-    }
-    
-    func openCliHelpWeb() {
-        NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/")! as URL)
     }
     
     @IBAction func askQuestionClicked(_ sender: NSMenuItem) {
-        openAskQuestionWeb()
-    }
-    
-    func openAskQuestionWeb() {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"ask-question")! as URL)
     }
     
-    @IBAction func linuxClicked(_ sender: NSMenuItem) {
-        openLinuxWeb()
-    }
     
-    func openLinuxWeb() {
+    @IBAction func linuxClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"questions/linux")! as URL)
     }
     
-    @IBAction func macosClicked(_ sender: NSMenuItem) {
-        openMacosWeb()
-    }
     
-    func openMacosWeb() {
+    @IBAction func macosClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"questions/macos")! as URL)
     }
     
-    @IBAction func networkingClicked(_ sender: NSMenuItem) {
-        openNetworkingWeb()
-    }
     
-    func openNetworkingWeb() {
+    @IBAction func networkingClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"questions/networking")! as URL)
     }
     
-    @IBAction func generalQClicked(_ sender: NSMenuItem) {
-        openGeneralQWeb()
-    }
     
-    func openGeneralQWeb() {
+    @IBAction func generalQClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"questions/general")! as URL)
     }
     
-    @IBAction func windowsClicked(_ sender: NSMenuItem) {
-        openWindowsWeb()
-    }
     
-    func openWindowsWeb() {
+    @IBAction func windowsClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"questions/windows")! as URL)
     }
     
-    @IBAction func helpAndSupportClicked(_ sender: NSMenuItem) {
-        openHelpAndSupportWeb()
-    }
     
-    func openHelpAndSupportWeb() {
+    @IBAction func helpAndSupportClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"help")! as URL)
     }
     
-    @IBAction func getInvolvedClicked(_ sender: NSMenuItem) {
-        openGetInvolvedWeb()
-    }
     
-    func openGetInvolvedWeb() {
+    @IBAction func getInvolvedClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"get-involved")! as URL)
     }
     
-    @IBAction func contactClicked(_ sender: NSMenuItem) {
-        openContactWeb()
-    }
     
-    func openContactWeb() {
+    @IBAction func contactClicked(_ sender: NSMenuItem) {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"contact-us")! as URL)
     }
     
     @IBAction func personalActivityClicked(_ sender: NSMenuItem) {
-        openPersonalActivityWeb()
-    }
-    
-    func openPersonalActivityWeb() {
         NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"user/activity")! as URL)
     }
     
+    
+    @IBAction func donateClicked(_ sender: NSMenuItem) {
+        NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"donate")! as URL)
+    }
+    
+    @IBAction func improveClicked(_ sender: NSMenuItem) {
+        NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/"+"clihelp-macos-app")! as URL)
+    }
+    
     @IBAction func cliHelpClicked(_ sender: NSMenuItem) {
-        openCliHelpWeb()
+        NSWorkspace.shared().open(NSURL(string: "https://community.clihelp.org/")! as URL)
     }
     
     
@@ -114,7 +84,4 @@ class StatusMenuController: NSObject, PreferencesWindowDelegate {
         NSApplication.shared().terminate(self)
     }
     
-    func preferencesDidUpdate() {
-        openCliHelpWeb()
-    }
 }
